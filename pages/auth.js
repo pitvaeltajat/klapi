6e6b8d704e26d2f22ea26c64b00d30cd3a7cd9ae
eerosahlberg <esahlber2004@gmail.com>
2022-04-22 17:40:50 +0300

    Installed and configured Chakra UI

--- pages/auth.js
+++ pages/auth.js
@@ -1,4 +1,5 @@
 import { useSession, signIn, signOut } from 'next-auth/react';
+import { Button, ButtonGroup } from '@chakra-ui/react';
 
 export default function Auth() {
     const { data: session } = useSession();
@@ -6,14 +7,14 @@ export default function Auth() {
         return (
             <>
                 Signed in as {session.user.email} <br />
-                <button onClick={() => signOut()}>Sign out</button>
+                <Button colorScheme='blue' onClick={() => signOut()}>Sign out</Button>
             </>
         );
     }
     return (
         <>
             Not signed in <br />
-            <button onClick={() => signIn()}>Sign in</button>
+            <Button colorScheme='blue' onClick={() => signIn()}>Sign in</Button>
         </>
     );
 }
