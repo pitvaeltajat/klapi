abdb41f38ecc762fb6ed4a777a4a48ec6fa261d2
petrus <holm.petrus@gmail.com>
2022-05-03 14:42:09 +0300

    Initial item view
    - Added item view
    - Added API route deleteItem
    - added ability to delete item in item view

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Container, Heading } from '@chakra-ui/react';
 import Auth from './auth';
-import NewItem from './newitem';
 import { PrismaClient } from '@prisma/client';
 import Link from 'next/link';
 
 const prisma = new PrismaClient();
 
-export async function getServerSideProps(context) {
+export async function getServerSideProps() {
     const items = await prisma.item.findMany();
     return {
         props: {
@@ -20,15 +19,19 @@ export default function Index({ items }) {
     return (
         <Container>
             <Auth />
-            <Heading>Kalusto App nimi pitäisi keksiä</Heading>
+            <Heading>KLAPI</Heading>
             <Link href='/newitem'>
                 <a>Luo uusi kama</a>
             </Link>
-            <ul>
-                {items.map((item) => (
-                    <li key={item.id}>{item.name}</li>
-                ))}
-            </ul>
+            <br />
+            {items.map((item) => (
+                <>
+                    <Link href={`/item/${item.id}`}>
+                        <a>{item.name}</a>
+                    </Link>
+                    <br />
+                </>
+            ))}
         </Container>
     );
 }
---
+++ pages/item/[id].js
@@ -0,0 +1,118 @@
+// get item by id and return it
+import { PrismaClient } from '@prisma/client';
+import React from 'react';
+import { useRouter } from 'next/router';
+import {
+    Editable,
+    EditableInput,
+    EditableTextarea,
+    EditablePreview,
+    Heading,
+    Button,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    useDisclosure,
+    useToast,
+} from '@chakra-ui/react';
+
+export async function getServerSideProps(req, res) {
+    const prisma = new PrismaClient();
+
+    const item = await prisma.item.findUnique({
+        where: {
+            id: req.params.id,
+        },
+    });
+    if (!item) {
+        return {
+            notFound: true,
+        };
+    }
+    return {
+        props: {
+            item,
+        },
+    };
+}
+
+export default function ItemView({ item }) {
+    const router = useRouter();
+    const toast = useToast();
+
+    const { isOpen, onOpen, onClose } = useDisclosure();
+
+    const [isEditing, setIsEditing] = React.useState(false);
+
+    const deleteItem = async () => {
+        // use api route to delete item
+        await fetch('/api/deleteItem', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(item.id),
+        })
+            .then(
+                toast({
+                    title: 'Success',
+                    description: 'Item deleted',
+                    status: 'success',
+                    duration: 5000,
+                    isClosable: true,
+                })
+            )
+            .catch((err) => {
+                toast({
+                    title: 'Error',
+                    description: err.message,
+                    status: 'error',
+                    duration: 5000,
+                    isClosable: true,
+                });
+            });
+        onClose();
+        // redirect to home page with router
+        router.push('/');
+    };
+
+    return (
+        <>
+            <Heading>{item.name}</Heading>
+            <Editable defaultValue={item.name}>
+                <EditablePreview />
+                <EditableInput />
+            </Editable>
+            <Editable defaultValue={item.description}>
+                <EditablePreview />
+                <EditableTextarea />
+            </Editable>
+            <Button onClick={() => setIsEditing(true)}>Muokkaa</Button>
+            <Button onClick={onOpen}>Poista</Button>
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Poistetaanko kama?</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <strong>{item.name}</strong> poistetaan. Oletko varma?
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button colorScheme='blue' mr={3} onClick={deleteItem}>
+                            Poista
+                        </Button>
+                        <Button colorScheme='gray' onClick={onClose}>
+                            Peruuta
+                        </Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+            <Heading>Varaustilanne</Heading>
+        </>
+    );
+}
