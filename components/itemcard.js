2a78f3eec538493727ea6531e99235037079f7c9
petrus <holm.petrus@gmail.com>
2022-05-04 16:41:33 +0300

    merge WIP-reservation-page to main

--- components/itemcard.js
+++ components/itemcard.js
@@ -1,23 +1,64 @@
-import { Button, Box, Stack, Flex } from "@chakra-ui/react"
-import NextLink from 'next/link'
-import { useDispatch, useSelector } from 'react-redux';
-import { addToCart } from '../redux/cart.slice';
+import {
+    Flex,
+    Box,
+    Image,
+    useColorModeValue,
+    IconButton,
+} from '@chakra-ui/react';
 
-const ItemCard = ({Item}) => {
-    const dispatch = useDispatch()
+import { FiShoppingCart } from '@chakra-ui/icons';
 
-    const cart = useSelector(state => state.cart)
-    console.log(cart)
+export default function ItemCard({ item }) {
+    return (
+        <Flex w='full' alignItems='center' justifyContent='center'>
+            <Box
+                bg={useColorModeValue('white', 'gray.800')}
+                maxW='sm'
+                borderWidth='1px'
+                rounded='lg'
+                shadow='lg'
+                position='relative'
+            >
+                <Image
+                    src={item.imageURL}
+                    alt={`Picture of ${item.name}`}
+                    roundedTop='lg'
+                    objectFit='cover'
+                    objectPosition='center'
+                    w='full'
+                    h='full'
+                    fallbackSrc='https://via.placeholder.com/300'
+                />
 
-    return(
-    <Box borderWidth='4px' borderRadius='10px' backgroundColor='gray.400' width='200px' padding='8px'>
-        <Box padding='4px'> <h2>{Item.name}</h2> </Box>
-        <Flex direction='row' alignContent='center'>
-            <Button colorScheme='blue' onClick={() => dispatch(addToCart(Item))}>Lisää koriin</Button>
-            <Box verticalAlign='center' p='2' borderRadius='full' backgroundColor='blue.500'>{cart.filter((item) => item.id === Item.id).map((item) => (item.quantity))}</Box>
-        </Flex>     
-    </Box>
-    )
+                <Box p='6'>
+                    <Flex
+                        mt='1'
+                        justifyContent='space-between'
+                        alignContent='center'
+                    >
+                        <Box
+                            fontSize='2xl'
+                            fontWeight='semibold'
+                            as='h4'
+                            lineHeight='tight'
+                            isTruncated
+                        >
+                            {item.name}
+                        </Box>
+
+                        <IconButton
+                            icon={FiShoppingCart}
+                            variant='transparent'
+                            onClick={() => {
+                                alert('Added to cart!');
+                            }}
+                            h={7}
+                            w={7}
+                            alignSelf={'center'}
+                        />
+                    </Flex>
+                </Box>
+            </Box>
+        </Flex>
+    );
 }
-
-export default ItemCard
