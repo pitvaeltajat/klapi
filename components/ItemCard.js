19dd7d6ba2efbcc72f5e8dfc0ae116bd4fdf5394
eerosahlberg <esahlber2004@gmail.com>
2022-05-21 13:53:58 +0300

    itemcard now uses api to check if it should the disabled

--- components/ItemCard.js
+++ components/ItemCard.js
@@ -35,7 +35,6 @@ export default function ItemCard({ item }) {
             .then(data => {
                 setAvailabilityData(data)
                 setLoading(false)
-                //calculateAvailability()
             }) 
     }, [])
 
@@ -54,52 +53,10 @@ export default function ItemCard({ item }) {
     const items = useSelector((state) => state.cart.items);
     const dates = useSelector((state) => state.dates);
 
-    /*
-    async function getAvailability(){
-        const startDate = dates.startDate
-        const endDate = dates.endDate
-        const body = {startDate, endDate, item}
-        const res = await fetch('api/getAvailability', {
-            method: 'POST',
-            headers: {
-            'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(body),
-            })
-            
-            .then(response => response.json())
-            .then(data => {
-                var availableAmount = data.availableAmount
-            })
-            
-        var data = await res.json()
-        console.log(data)
-        if(res){
-            calculateAvailability(data)
-        }
-            
-
-    }
-    */
-
     if (isLoading) return <p>Ladataan...</p>
     if (!availabilityData) return <p>Error</p>
     
     let itemDisabled = false;
-    /*
-    function calculateAvailability(){
-        if (
-            availabilityData.availableAmount -
-                items
-                    .filter((filterItem) => filterItem.id === item.id)
-                    .map((item) => item.amount) <= 0
-        ) {
-            itemDisabled = true;
-        } else {
-            itemDisabled = false;
-        }
-    }
-    */
 
     return (
         <Flex w='full' alignItems='center' justifyContent='center'>
@@ -138,16 +95,19 @@ export default function ItemCard({ item }) {
                             {item.name}
                             {availabilityData.availableAmount}
                         </Box>
-                        {!itemDisabled ? (
-                            <Button
-                                onClick={() => addItemToCart(item)}
-                                h={7}
-                                w={7}
-                                alignSelf={'center'}
-                            >
-                                Lisää
-                            </Button>
-                        ) : null}
+                        {availabilityData.availableAmount - 
+                        items.filter((filterItem) => filterItem.id === item.id)
+                        .map((item) => item.amount) > 0
+                            ? (
+                                <Button
+                                    onClick={() => addItemToCart(item)}
+                                    h={7}
+                                    w={7}
+                                    alignSelf={'center'}
+                                >
+                                    Lisää
+                                </Button>
+                            ) : null}
                         {items
                             .filter((cartItem) => cartItem.id === item.id)
                             .map((cartItem) => cartItem.amount)}
