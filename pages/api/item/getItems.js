ddc38d35d12152f3e4348a99946be7bfa94a465a
eerosahlberg <esahlber2004@gmail.com>
2022-05-30 15:49:13 +0300

    Restricted api usage for non-admin users

--- pages/api/item/getItems.js
+++ pages/api/item/getItems.js
@@ -1,6 +1,15 @@
 import prisma from '/utils/prisma';
+import { getSession } from 'next-auth/react';
 
 export default async function handler(req, res) {
+    const session = await getSession({ req });
+    console.log('session in API: ', session);
+    if (session?.user?.group !== 'ADMIN') {
+        res.status(401).json({
+            message: 'Sinulla ei ole oikeutta tähän toimintoon',
+        });
+    }
+    
     const items = await prisma.item.findMany({include: {
         categories: true,
         reservations: { include: { loan: true } },
