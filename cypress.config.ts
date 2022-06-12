bcc3aaf583c2db95d99f1c7a46ddefba3018b20f
petrus <holm.petrus@gmail.com>
2022-06-12 11:27:46 +0300

    add tsconfig, modified cypress config to work with nextjs

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  component: {
-    devServer: {
-      framework: "next",
-      bundler: "webpack",
+    component: {
+        devServer: {
+            framework: 'next',
+            bundler: 'webpack',
+        },
     },
-  },
-
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    e2e: {
+        baseUrl: 'http://localhost:3000',
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
     },
-  },
 });
