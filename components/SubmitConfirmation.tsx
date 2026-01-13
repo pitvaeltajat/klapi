f4930622218dd8521a3bac2b55eb369beec14d9c
eerosahlberg <esahlber2004@gmail.com>
2026-01-13 23:06:25 +0200

    pls work

--- components/SubmitConfirmation.tsx
+++ components/SubmitConfirmation.tsx
@@ -16,15 +16,11 @@ import {
 	useToast,
 } from '@chakra-ui/react';
 import React from 'react';
+import { CartItem } from '../types';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import { useCart } from '@/contexts/CartContext';
 import { useDates } from '@/contexts/DatesContext';
-
-interface CartItem {
-	id: string;
-	amount: number;
-}
 
 export default function SubmitConfirmation({
 	isOpen,
